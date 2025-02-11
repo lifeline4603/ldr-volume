0cd0c86ad8c455c723a787683c468ce2ac1994eb
Simao <135634664+lifeline4603@users.noreply.github.com>
2025-02-11 18:00:25 +0100

    better

--- main.ts
+++ main.ts
@@ -1,16 +1,34 @@
+let lightlevel2 = 0
+let maxlevels = 0
+let brightness = 0
 let lightlevel = 0
-let ledamount = 0
-let x = 0
-let y = 0
-basic.forever(function () {
-    lightlevel = pins.analogReadPin(AnalogPin.P0)
-    ledamount = Math.map(lightlevel, 0, 1023, 0, 25)
-    ledamount = Math.round(ledamount)
+input.onButtonPressed(Button.A, function () {
+    lightlevel2 = pins.analogReadPin(AnalogPin.P0)
+    basic.showNumber(lightlevel2)
+    basic.pause(1000)
     basic.clearScreen()
-    for (let i = 0; i <= ledamount - 1; i++) {
-        x = i % 5
-        y = 4 - Math.floor(i / 5)
-        led.plot(x, y)
+    drawBarGraph(lightlevel2)
+})
+function drawBarGraph (value: number) {
+    // number doesnt actually error not sure why it shows up as one
+    maxlevels = 5
+    brightness = Math.map(value, 0, 1023, 0, 25)
+    for (let y = 0; y <= maxlevels - 1; y++) {
+        for (let x = 0; x <= 4; x++) {
+            if (y * 5 + x < Math.floor(brightness)) {
+                led.plot(x, 4 - y)
+            } else {
+                led.unplot(x, 4 - y)
+            }
+        }
     }
-    basic.pause(500)
+}
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.clearScreen()
+        basic.pause(4000)
+    } else {
+        lightlevel = pins.analogReadPin(AnalogPin.P0)
+        drawBarGraph(lightlevel)
+    }
 })
